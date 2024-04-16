9b003b5c4f9c295ab13928b6d07d499ef4cbf591
Fajar Akmal <77359459+s4s0r1@users.noreply.github.com>
2024-04-17 05:23:30 +0700

    Update subscribe.js

--- subscribe.js
+++ subscribe.js
@@ -1,15 +1,12 @@
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
-
 // Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCvSb4Z09Cb6GgN-oCI05tsTpA8oWlbLCo",
-    authDomain: "broadcastnotification-80fd9.firebaseapp.com",
-    projectId: "broadcastnotification-80fd9",
-    storageBucket: "broadcastnotification-80fd9.appspot.com",
-    messagingSenderId: "1001002027993",
-    appId: "1:1001002027993:web:07d83621b8ea73b19967c8",
-    measurementId: "G-G2C0KXVS09"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID"
 };
 
 // Function to request notification permission
@@ -19,11 +16,11 @@ function requestPermission() {
         if (permission === 'granted') {
             console.log('Notification permission granted.');
             // Initialize Firebase
-            const app = initializeApp(firebaseConfig);
+            const app = firebase.initializeApp(firebaseConfig);
 
             // Initialize Firebase Cloud Messaging and get a reference to the service
-            const messaging = getMessaging(app);
-            getToken(messaging, { vapidKey: 'BGdt-g4are3Pd6K6z6OxNjwYEP-jwJwpPV_9EdusGbjOFUCjV6DvYiR8dD03UNYwNT3sCsZ8tY7ZaUsGe0WiV28' })
+            const messaging = firebase.messaging(app);
+            messaging.getToken({ vapidKey: 'BGdt-g4are3Pd6K6z6OxNjwYEP-jwJwpPV_9EdusGbjOFUCjV6DvYiR8dD03UNYwNT3sCsZ8tY7ZaUsGe0WiV28' })
                 .then((currentToken) => {
                     if (currentToken) {
                         console.log('currentToken: ', currentToken);
@@ -33,7 +30,7 @@ function requestPermission() {
                 });
 
             // Listen for incoming messages
-            onMessage(messaging, (payload) => {
+            messaging.onMessage((payload) => {
                 console.log('Message received. ', payload);
                 // Handle the message and display the notification
                 const notificationTitle = payload.notification.title;
